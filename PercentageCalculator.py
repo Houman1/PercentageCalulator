33fdc9f821340755ddd28cdddf17aeff4023c433
Houman <houman@hotmail.co.uk>
2021-01-03 02:44:00 +0000

    Update PercentageCalculator.py

--- PercentageCalculator.py
+++ PercentageCalculator.py
@@ -1,3 +1,5 @@
+import re
+
 
 dictionary = {}
 
@@ -7,8 +9,11 @@ NumOfItems= int(input("How many items do you have?: "))
 for i in range(NumOfItems):
     item = input("please input the name of your item: ")
     value = input("please input the value of your item: ")
-    dictionary[item] = value
+    while not re.match(r'^([\s\d]+)$', value):
+        print("value is not valid format, please use integers")
+        value = input("please input the value of your item: ")
 
+    dictionary[item] = value
 
 
 
@@ -17,7 +22,9 @@ Total_amount = 0
 for i in dictionary:
     Total_amount += int(dictionary[i])
 
-print("\nThe percentage of each item is: ")
+print("\n\n\nThe percentage of each item is: ")
 
 for i in dictionary:
     print(f"{i} = {round(int(dictionary[i])/Total_amount*100)}%")
+
+input("\n\n\nPress enter to exit")
