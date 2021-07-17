4956585fa33e39ad6b31743a33229760b690a2b9
Houman <houman@hotmail.co.uk>
2021-07-17 02:56:06 +0100

    add recent changes

--- PercentageCalculator.py
+++ PercentageCalculator.py
@@ -7,8 +7,8 @@ NumOfItems= int(input("How many items do you have?: "))
 
 
 for i in range(NumOfItems):
-    item = input("please input the name of your item: ")
-    value = input("please input the value of your item: ")
+    item = input("please input item NAME: ")
+    value = input("please input the VALUE of your item: ")
     while not re.match(r'^([\s\d]+)$', value):
         print("value is not valid format, please use integers")
         value = input("please input the value of your item: ")
@@ -17,14 +17,31 @@ for i in range(NumOfItems):
 
 
 
-Total_amount = 0
+def percentages(dictionary):
+    Total_amount = 0
 
-for i in dictionary:
-    Total_amount += int(dictionary[i])
+    for i in dictionary:
+        Total_amount += int(dictionary[i])
 
-print("\n\n\nThe percentage of each item is: ")
+    print("\n\n\nThe percentage of each item is: ")
 
-for i in dictionary:
-    print(f"{i} = {round(int(dictionary[i])/Total_amount*100)}%")
 
-input("\n\n\nPress enter to exit")
+
+    for i in dictionary:
+        print(f"{i} = {round(int(dictionary[i])/Total_amount*100)}%")
+
+
+
+
+    num = input("\n\n\nEnter total food eaten:")
+
+    for i in dictionary:
+        print(f"{i} = {round(int(dictionary[i]) / Total_amount * int(num))}")
+
+
+
+    input("\n\n\nPress enter to exit")
+
+percentages(dictionary)
+
+
