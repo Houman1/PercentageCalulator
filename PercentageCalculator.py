db7e3cc7a394494115cf25d9ae563fb7b42e8dd6
Houman <houman@hotmail.co.uk>
2022-05-14 22:16:19 +0100

    put Percentage calculator into class, only ask for save if not loaded a meal

--- PercentageCalculator.py
+++ PercentageCalculator.py
@@ -1,67 +1,70 @@
 import re
 import save_meal
 
-save = save_meal.SaveMeal()
-dictionary = {}
 
-def new_meal():
-    load_meal_choice = input("Do you want to load a meal? (Y/N)\n")
+class PercentageCalculator:
 
-    if load_meal_choice.lower() == "y":
-        save.show_meals()
-        load_meal_number = int(input("which meal number would you like?\n"))
-        loaded_meal = save.load_meal(load_meal_number)
-        print(loaded_meal)
-        dictionary=loaded_meal
+    def __init__(self):
+        self.save = save_meal.SaveMeal()
+        self.dictionary = {}
+        self.load_meal_choice = "n"
 
-    else:
-        NumOfItems = int(input("How many items do you have?: "))
+    def new_meal(self):
+        load_meal_choice = input("Do you want to load a meal? (Y/N)\n")
+        self.load_meal_choice = load_meal_choice
 
-        for i in range(NumOfItems):
-            item = input("please input item NAME: ")
-            value = input("please input the VALUE of your item: ")
-            while not re.match(r'^([\s\d]+)$', value):
-                print("value is not valid format, please use integers")
-                value = input("please input the value of your item: ")
+        if load_meal_choice.lower() == "y":
+            self.save.show_meals()
+            load_meal_number = int(input("which meal number would you like?\n"))
+            loaded_meal = self.save.load_meal(load_meal_number)
+            print(loaded_meal)
+            self.dictionary = loaded_meal
 
-            dictionary[item] = value
+        else:
+            NumOfItems = int(input("How many items do you have?: "))
 
+            for i in range(NumOfItems):
+                item = input("please input item NAME: ")
+                value = input("please input the VALUE of your item: ")
+                while not re.match(r'^([\s\d]+)$', value):
+                    print("value is not valid format, please use integers")
+                    value = input("please input the value of your item: ")
 
-def percentages(dictionary):
-    total_amount = 0
+                self.dictionary[item] = value
 
-    for i in dictionary:
-        total_amount += int(dictionary[i])
+    def percentages(self):
+        total_amount = 0
 
-    print("\n\n\nThe percentage of each item is: ")
+        for i in self.dictionary:
+            total_amount += int(self.dictionary[i])
 
-    for i in dictionary:
-        print(f"{i} = {round(int(dictionary[i]) / total_amount * 100)}%")
+        print("\n\n\nThe percentage of each item is: ")
 
-    num = int(input("\n\n\nEnter total food eaten:"))
+        for i in self.dictionary:
+            print(f"{i} = {round(int(self.dictionary[i]) / total_amount * 100)}%")
 
-    while type(num) != int:
-        print("That is not a valid integer value, please enter an integer value")
-        num = input("\n\n\nEnter total food eaten:")
+        num = int(input("\n\n\nEnter total food eaten:"))
 
-    for i in dictionary:
-        print(f"{i} = {round(int(dictionary[i]) / total_amount * int(num))}")
+        while type(num) != int:
+            print("That is not a valid integer value, please enter an integer value")
+            num = input("\n\n\nEnter total food eaten:")
 
-    print("*"*50 + "\n")
+        for i in self.dictionary:
+            print(f"{i} = {round(int(self.dictionary[i]) / total_amount * int(num))}")
 
-new_meal()
-percentages(dictionary)
+        print("*" * 50 + "\n")
 
-def replay_or_save():
-    replay = input("do you want to do another quantity of this meal (Y/N)?" + "\n")
-
-    while replay.lower() == "y":
-        percentages(dictionary)
+    def replay_or_save(self):
         replay = input("do you want to do another quantity of this meal (Y/N)?" + "\n")
 
-    save.choose()
-    2
-    if save.choice == "y":
-        save.save_meal(foods=dictionary)
+        while replay.lower() == "y":
+            self.percentages()
+            replay = input("do you want to do another quantity of this meal (Y/N)?" + "\n")
 
-    input("\nPress enter to exit")
+        if self.load_meal_choice == "n":
+            self.save.choose()
+
+            if self.save.choice == "y":
+                self.save.save_meal(foods=self.dictionary)
+
+        input("\nPress enter to exit")
---
+++ main.py
@@ -0,0 +1,8 @@
+import PercentageCalculator
+
+
+meal = PercentageCalculator.PercentageCalculator()
+
+meal.new_meal()
+meal.percentages()
+meal.replay_or_save()
